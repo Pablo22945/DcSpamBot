1e055f92565f1d13106e7edf98f6849e6f107e8f
Pablo22945 <adolfhitler4584@gmail.com>
2024-11-13 02:00:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,18 +7,18 @@ load_dotenv()
 
 # FOR CODES
 
-API_ID = int(getenv("API_ID", "25206101" )) 
-API_HASH = getenv("API_HASH", "2135724a8fdecb737f31d22ec8e6894b" ) 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7919159999" ).split()))
+API_ID = int(getenv("API_ID", )) 
+API_HASH = getenv("API_HASH",  ) 
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", ).split()))
 LOGGER = int(getenv("LOGGER", None)) 
-OWNER = int(getenv("OWNER_ID", "6062173301" )) 
+OWNER = int(getenv("OWNER_ID", )) 
 NAME = getenv("ALIVE_NAME")
 OWN_USERNAME= getenv("OWN_USERNAME")
 ALIVE_PIC = getenv("ALIVE_PIC") 
 
 # FOR SPAMBOT
 
-TOKEN1 = getenv("TOKEN1", "7759714753:AAFbvUfyUOWiPsB_uoymbf5DYlCADCuIZJo" None) 
+TOKEN1 = getenv("TOKEN1", None) 
 TOKEN2 = getenv("TOKEN2", None) 
 TOKEN3 = getenv("TOKEN3", None) 
 TOKEN4 = getenv("TOKEN4", None) 
